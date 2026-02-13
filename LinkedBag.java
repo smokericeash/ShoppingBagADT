33d087a442680bd54b2db2d51ebda9c487665312
smokericeash <aidanchenn1@gmail.com>
2026-02-12 23:02:52 -0800

    made changes and added more structure to linked bag

--- LinkedBag.java
+++ LinkedBag.java
@@ -1,5 +1,5 @@
 public class LinkedBag<T> implements BagInterface<T>{
-    Node<T> firstNode;
+    Node firstNode;
     int numberOfEntries;
 
     /*gets current number of items in this bag 
@@ -19,21 +19,46 @@ public class LinkedBag<T> implements BagInterface<T>{
      @param newEntry The object to be added is a newEntry
      @return if adding was successful, false if failure*/
     public boolean add(T newEntry){
-        boolean result = false;
+        Node newNode = new Node(newEntry);
+
+        newNode.next = firstNode;
+
+        firstNode = newNode;
+        numberOfEntries++;
+
+        return true;
 
     }
 
     /*removes any item from the bag
      @returns the removed entry or if the removal was successful or if empty, return null*/
     public T remove(){
-
+        T result = null;
+        if (firstNode!=null){
+            result = firstNode.getData();
+            firstNode=firstNode.next;
+            numberOfEntries--;
+        }
+        return result;
     }
 
     /* removes a specific item from bag
      @param anEntry, the object to be removed is anEntry
      @returns true if successful, false if failure*/
     public boolean remove(T anEntry){
+        boolean result = false;
 
+        Node nodeN = getReferenceTo(anEntry);
+        if(nodeN !=null){
+            //replace located entry with entry in first
+            nodeN.setData(firstNode.getData());
+            firstNode = firstNode.next;
+
+            numberOfEntries--;
+   
+            result = true; 
+        }
+        return result;
     }
 
     /* removes all entries in bag
@@ -53,13 +78,33 @@ public class LinkedBag<T> implements BagInterface<T>{
      @param anEntry, the item that's checked to see if the bag contains it
      @returns true if the bag contains, false if not*/
     public boolean contains(T anEntry){
+        boolean found = false;
+        Node currentNode = firstNode;
 
+        while(!found && (currentNode.next!=null)){
+            if(anEntry.equals(currentNode.getData())){
+                found = true;
+            }
+            else{
+                currentNode = currentNode.next;
+            }
+        }
+        return found;
     }
 
     /* looks at all objects that are within the bag
      @returns a newly allocated array of all entires inside the bag, if array is empty then it will return another empty array*/
+     @SuppressWarnings("unchecked")
     public T[] toArray(){
-
+        T[] bag = (T[])new Object[numberOfEntries];
+        Node currentNode = firstNode;
+        int index = 0;
+        while(currentNode!=null){
+            bag[index] = currentNode.getData();
+            index++;
+            currentNode = currentNode.next;
+        }
+        return bag;
     }
 
     public LinkedBag(){
@@ -68,17 +113,38 @@ public class LinkedBag<T> implements BagInterface<T>{
     }
 
 
+    Node getReferenceTo(T anEntry){
+            boolean found = false;
+            Node currentNode = firstNode;
+
+            while(!found && (currentNode!=null)){
+                if(anEntry.equals(currentNode.getData())){
+                    found = true;
+                }
+                else{
+                    currentNode = currentNode.next;
+                }
+            }
+            return currentNode;
+
+        }
 
 
-
-
-    private static class Node<T>{
+    private class Node{
         T data;
-        Node<T> next;
+        Node next;
 
         Node(T data){
             this.data = data;
             this.next = null;
+        }
+
+        T getData(){
+            return data;
+        }
+
+        private void setData(T data){
+            this.data = data;
         }
 
 
